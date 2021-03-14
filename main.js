7a753ef416c07d57ee1bbac8258d87121e04f490
mysccafrte <80043850+mysccafrte@users.noreply.github.com>
2021-03-14 18:28:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-// кажется тут должен быть код...
-
-// Подсказка №1: Содержимое тега textarea хранится в его свойстве value
-
-// Подсказка №2: Не забывайте, что LocalStorage и SessionStorage могут хранить только строки в виде пар ключ/значение
+clicker = function(){
+    let p = document.createElement('p');
+    p.innerText = document.querySelector('textarea').value;
+    document.querySelector('.news__brief').append(p);
+    localStorage.setItem(`comment ${localStorage.getItem('i')}`, p.innerText);
+    localStorage.setItem(`i`, `${Number(localStorage.getItem('i'))+1}`)
+}
+if(localStorage.getItem(`i`)==undefined) localStorage.setItem(`i`, '1');
+for(let j = 1; j < localStorage.length;j++){
+    let tex = document.createElement('p');
+    tex.innerText = localStorage.getItem(`comment ${j}`);
+    document.querySelector('.news__brief').append(tex);
+}
+document.querySelector('button').addEventListener('click', clicker);
